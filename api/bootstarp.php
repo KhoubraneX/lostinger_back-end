2cf7e755874a9ff0d816a8adfc4a0c4ba6dae02e
x <maroine619@gmail.com>
2023-04-17 17:55:43 +0000

    register

--- api/bootstarp.php
+++ api/bootstarp.php
@@ -3,6 +3,7 @@ require_once  dirname(__DIR__) . "/vendor/autoload.php";
 require dirname(__DIR__) . "/src/functions/__built_in_functions.php";
 require dirname(__DIR__) . "/src/util/JWTCodec.php";
 require dirname(__DIR__) . "/src/classes/ItemGateway.php";
+require dirname(__DIR__) . "/src/classes/ItemDetailsGateway.php";
 require dirname(__DIR__) . "/src/classes/UserGateway.php";
 
 // handel errors by json format
@@ -12,5 +13,4 @@ set_exception_handler("ErrorHandler::handleException");
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
-header('Content-Type: application/json; charset=utf-8');
 ?>
--- api/index.php
+++ api/index.php
@@ -1,5 +1,15 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if ($_SERVER["REQUEST_METHOD"] == "OPTIONS") {
+  http_response_code(200);
+  exit();
+}
+
 require dirname(__DIR__) . "/api/bootstarp.php";
 
 $path = explode("/" , parse_url($_SERVER["REQUEST_URI"] , PHP_URL_PATH));
@@ -8,14 +18,18 @@ $resource = $path[3];
 $id = $path[4] ?? null;
 $method = $_SERVER["REQUEST_METHOD"];
 
-if ($resource !== "items" && $resource !== "auth") {
+if ($resource !== "items" && $resource !== "auth" && $resource !== "itemDetails") {
     notFound();
     exit;
 }
 
 switch ($resource) {
     case 'items':
-        $controller = new ItemController(0);
+        $controller = new ItemController();
+        $controller->processRequest($method , $id);
+        break;
+    case 'itemDetails':
+        $controller = new ItemDetailsController;
         $controller->processRequest($method , $id);
         break;
     case 'auth':
